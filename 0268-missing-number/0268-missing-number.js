fdb098dbf8b931ed2b8d690fc0fafbf882974d46
hakim-9895 <abdulhakim9895@gmail.com>
2025-01-16 09:46:52 +0530

    Time: 17 ms (28.81%), Space: 51.4 MB (54.95%) - LeetHub

--- 0268-missing-number/0268-missing-number.js
+++ 0268-missing-number/0268-missing-number.js
@@ -3,15 +3,10 @@
  * @return {number}
  */
 var missingNumber = function(nums) {
-    let sorted=nums.sort((a,b)=>a-b)
-    for(let i=0;i<=nums.length;i++){
-        if(nums[i]!==i){
-            return i
-        }
-        if(nums.length==0){
-            return 0
-        }
-        
+    nums.sort((a,b)=>a-b)
+for (let i= 0;i<=nums.length;i++){
+    if(i!==nums[i]){
+         return i 
     }
-   
-};
+}
+}
